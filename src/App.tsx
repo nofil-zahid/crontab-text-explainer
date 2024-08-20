c55f5758a09513003990c1ea05730de2fc5d4170
nofil-zahid <chmnofilzahidofficial85p@gmail.com>
2024-08-20 15:36:13 +0500

    small changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,10 @@ import InputField from './components/InputField/InputField';
 import TextArea from './components/TextArea/TextArea';
 import style from './App.css';
 import Button from './components/Button/Button';
+import Footer from './layout/Footer/Footer';
 
 function App() {
   const [value, setValue] = useState<string>("0 22 * * 1-5");
-  const [currentIndex, setCurrentIndex] = useState<number>(0);
-  // let val = 0;
-
-  const randomCronValues = () => {
-    const valArr = ["0 2 * 1,2,3 1,5", "5 4 * * 0", "0 22 * * 1-5", "0 0 1,15 * 3", "5 0 * 8 *"];
-    setValue(valArr[currentIndex]);
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % valArr.length);
-  }
 
   return (
     <div style={style.container}>
@@ -22,8 +15,9 @@ function App() {
       <TextArea text={value} />
       <span style={{display: 'flex', flexDirection: 'row'}}>
         <InputField value={value} setValue={setValue}/>
-        <Button callbackFunc={randomCronValues}/>
+        <Button callbackFunc={setValue}/>
       </span>
+      <Footer/>
     </div>
   )  
 }
