3a0572a81db5f3e56e96734abe0e26a8312551a2
nofil-zahid <chmnofilzahidofficial85p@gmail.com>
2024-08-20 15:30:59 +0500

    font changes

--- src/layout/Header/Header.css.ts
+++ src/layout/Header/Header.css.ts
@@ -2,22 +2,22 @@ import { Styles } from '../../interfaces/Styling';
 
 const styles: Styles = {
     header1: {
-        fontSize: 50,
+        fontSize: 60,
+        fontWeight: "bold",
+        fontFamily: 'cursive',
         margin: 0,
     },
     header: {
-        color: '#E0E0E0', // Soft white for text
-        fontSize: '1rem', // Adjusted for better readability
+        color: '#E0E0E0',
+        fontSize: '1rem',
         textAlign: 'center',
         marginTop: '10px',
     },
     center_div: {
-        display: 'flex',
-        // alignItems: 'center',
-        // justifyContent: 'center',    
+        display: 'flex',   
         flexDirection: 'column',
-        color: '#00bfa5', // Teal color for additional text
-        margin: '20px 0', // Added margin for spacing
+        color: '#00bfa5',
+        margin: '20px 0',
     },
 };
 
