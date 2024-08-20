5e7e2779846dd39d2690811a7f45bfcbefe5f733
nofil-zahid <chmnofilzahidofficial85p@gmail.com>
2024-08-20 15:34:52 +0500

    new changes

--- src/App.css.ts
+++ src/App.css.ts
@@ -6,11 +6,13 @@ const style : Styles = {
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'center',
+        // paddingTop: 100,
         flexDirection: 'column',
         backgroundColor: '#212121',
         width: "100%",
         height: '100vh',
         padding: '20px',
+        fontFamily: "monospace"
     }
 }
 
