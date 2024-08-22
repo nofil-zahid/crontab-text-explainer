0f4cf268c87625de34d40e8b374f1d55f12b609b
nofil-zahid <chmnofilzahidofficial85p@gmail.com>
2024-08-22 14:13:36 +0500

    lil changes for rspnsv

--- src/layout/Footer/Footer.css.ts
+++ src/layout/Footer/Footer.css.ts
@@ -4,18 +4,18 @@ const footerStyle: Styles = {
     footerContainer: {
         width: '100%',
         backgroundColor: '#424242', // Dark gray background
-        padding: '10px 0',
+        padding: 2,
         position: 'fixed',
         bottom: 0,
         left: 0,
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
+        // display: 'flex',
+        // justifyContent: 'center',
+        // alignItems: 'center',
         borderTop: '1px solid #616161', // Slightly lighter gray for the border
     },
     footerText: {
         color: '#E0E0E0', // Soft white text
-        fontSize: '1rem',
+        fontSize: '0.7rem',
         textAlign: 'center',
     },
 };
--- src/layout/Header/Header.css.ts
+++ src/layout/Header/Header.css.ts
@@ -6,18 +6,21 @@ const styles: Styles = {
         fontWeight: "bold",
         fontFamily: 'cursive',
         margin: 0,
+        textAlign: 'center'
     },
     header: {
         color: '#E0E0E0',
         fontSize: '1rem',
         textAlign: 'center',
         marginTop: '10px',
+        textWrap: 'wrap'
     },
     center_div: {
         display: 'flex',   
         flexDirection: 'column',
         color: '#00bfa5',
         margin: '20px 0',
+        maxWidth: innerWidth-100
     },
 };
 
